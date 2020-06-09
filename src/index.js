23cb62fe2cdca381b47c7ffb94fa9677789defc9
imtiaz051 <imtiazalisoomro@gmail.com>
2020-06-09 20:14:00 +0500

    index.js
    change descriptions

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+//import React from 'react';
+//import ReactDOM from 'react-dom';
 //import './index.css';
 //import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 
-const myfirstelement = <h1>Helleo react</h1>
-ReactDOM.render(myfirstelement,
+//const myfirstelement = <h1>Helleo react</h1>
+//ReactDOM.render(myfirstelement,
   
-  document.getElementById('root')
-);
+  //document.getElementById('root')
+//);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
 //<React.StrictMode>
 //<App />
 //</React.StrictMode>,
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<div><h1>React First Element</h1></br><p>This
+   is my first element created by me</p></div>,document.getElementById("root"));
+   serviceWorker.unregister();
