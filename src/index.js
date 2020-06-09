1025c6a3d4ce3c4452094b84545bb6527fb9b3e7
imtiaz051 <imtiazalisoomro@gmail.com>
2020-06-09 12:03:11 +0500

    update index,js
    import serviceworker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
 //import App from './App';
-//import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 
 const myfirstelement = <h1>Helleo react</h1>
 ReactDOM.render(myfirstelement,
